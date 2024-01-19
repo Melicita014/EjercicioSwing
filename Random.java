ac0c684d8b388d8866a62ac400f32b1bf810c12e
Melicita014 <145325113+Melicita014@users.noreply.github.com>
2024-01-19 08:48:49 +0100

    Random.java

--- Random.java
+++ Random.java
@@ -1,4 +1,5 @@
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -6,29 +7,47 @@ import javax.swing.*;
 
 public class Random extends JFrame implements ActionListener {
 	JButton btnRandom ;
-	JLabel lbletiqueta1 , lbletiqueta2, lbletiqueta3 , lblResultado;
-	JLabel lblMensaje , lblMensaje2 , lblMensaje3 ;
+	
+	JLabel lbletiqueta1 , lbletiqueta2, lbletiqueta3 , lblResultado; //Para los numeros.
+	JLabel lbletiqueta4 , lbletiqueta5, lbletiqueta6 , lblResultado1; // Para las fotos.
+	
+	ImageIcon imgGatos , imgPerros , imgCaballo;
 	
 	Random(){
-		setLayout (new FlowLayout());
+		setLayout (new GridLayout(6,9));
+	
+	//Fotitos:	
+		imgGatos = new ImageIcon ("e:\\cat.jpg");
+		imgPerros = new ImageIcon ("e:\\perro.jpg");
+		imgCaballo = new ImageIcon ("e:\\Caballo.jpg");
 		
-		btnRandom = new JButton ("RANDOM");
-		
+		btnRandom = new JButton ("RANDOM"); // Boton para las dos
+	
+	//Numeros:
 		lbletiqueta1 = new JLabel ();
 		lbletiqueta2 = new JLabel ();
 		lbletiqueta3 = new JLabel ();
 		
 		lblResultado = new JLabel();
 		
-		//lblMensaje = new JLabel ("Los tres valores son diferentes");
-	//	lblMensaje2 = new JLabel ("Hay dos valores iguales");
-	//	lblMensaje3 = new JLabel ("Los tres valores son iguales");
+	//Fotos:	
+		lbletiqueta4 = new JLabel ();
+		lbletiqueta5 = new JLabel ();
+		lbletiqueta6 = new JLabel ();
+		
+		lblResultado1 = new JLabel();
 		
 		add(btnRandom);
+		
 		add(lbletiqueta1);
 		add(lbletiqueta2);
 		add(lbletiqueta3);
 		add(lblResultado);
+		
+		add(lbletiqueta4);
+		add(lbletiqueta5);
+		add(lbletiqueta6);
+		add(lblResultado1);
 		
 		btnRandom.addActionListener(this);
 	
@@ -42,33 +61,81 @@ public class Random extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource()== btnRandom) {
+		
+			//Numero aleatorio para los numeros:
+			
 			int	random1 = (int) Math.floor(Math.random()*100) ; 
 			int	random2 = (int) Math.floor(Math.random()*100) ; 
 			int	random3 = (int) Math.floor(Math.random()*100) ; 
 			
+			//Numero aleatorio para las fotos:
 			
+			int randomFoto1 = (int) Math.floor(Math.random()*3);
+			int randomFoto2 = (int) Math.floor(Math.random()*3);
+			int randomFoto3 = (int) Math.floor(Math.random()*3);
+			
+		// Ponemos a cada etiqueta una foto segun el numero que toque.	
+			
+			if(	randomFoto1 == 1)
+				lbletiqueta4.setIcon(imgGatos);
+			
+			if(	randomFoto1==2)
+				lbletiqueta4.setIcon(imgPerros);
 		
+			if(	randomFoto1 ==3)
+				lbletiqueta4.setIcon(imgCaballo);
+			
+			
+			if(	randomFoto2 == 1)
+				lbletiqueta5.setIcon(imgGatos);
+			
+			if(	randomFoto2==2)
+				lbletiqueta5.setIcon(imgPerros);
+			
+			if(	randomFoto2 ==3)
+				lbletiqueta5.setIcon(imgCaballo);
+			
+			
+			if(	randomFoto3 == 1)
+				lbletiqueta6.setIcon(imgGatos);
+			
+			if(	randomFoto3==2)
+				lbletiqueta6.setIcon(imgPerros);
+			
+			if(	randomFoto3 ==3)
+				lbletiqueta6.setIcon(imgCaballo);
+			
+			    lbletiqueta1.setText(String.valueOf(randomFoto1)); 
+				lbletiqueta2.setText(String.valueOf(randomFoto2));
+				lbletiqueta3.setText(String.valueOf(randomFoto3));
+				
 		    lbletiqueta1.setText(String.valueOf(random1)); 
 			lbletiqueta2.setText(String.valueOf(random2));
 			lbletiqueta3.setText(String.valueOf(random3));
 	
-		
+		//FOTOS:
+
 			
-			if (random1==random2 & random1==random3) 
-					lblResultado.setText("Los TRES valores son IGUALES" );
+			if (randomFoto1==randomFoto2 & randomFoto1==randomFoto3) 
+					lblResultado1.setText("Los TRES valores son IGUALES" );
 			else
-				if (random1 == random2 || random1==random3 || random2 ==random3 )
-					lblResultado.setText("Hay DOS valores IGUALES");
+				if (randomFoto1 == randomFoto2 || randomFoto1==randomFoto3 || randomFoto2 ==randomFoto3 )
+					lblResultado1.setText("Hay DOS valores IGUALES");
 				else 
-					if (random1 != random2 || random1!=random3 || random2 !=random3)
-					lblResultado.setText("Los TRES valores son DIFERENTES");
-					
-		
+					if (randomFoto1 != randomFoto2 || randomFoto1!=randomFoto3 || randomFoto2 !=randomFoto3)
+					lblResultado1.setText("Los TRES valores son DIFERENTES");
+			
+		//NUMEROS:			
+					if (random1==random2 & random1==random3) 
+				lblResultado.setText("Los TRES valores son IGUALES" );
+		else
+			if (random1 == random2 || random1==random3 || random2 ==random3 )
+				lblResultado.setText("Hay DOS valores IGUALES");
+			else 
+				if (random1 != random2 || random1!=random3 || random2 !=random3)
+				lblResultado.setText("Los TRES valores son DIFERENTES");
 		}
 		
-
-		
-
 	}
 
 
